9e9f575bb6fd7b4e11848d680977896707c37d9e
Rob de Boer <roblkdeboer@gmail.com>
2021-07-26 16:43:50 +0800

    Clean up readme

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -8,7 +8,6 @@ const Product = ({ product }) => {
       <a href={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top ' />
       </a>
-
       <Card.Body>
         <a href={`/product/${product._id}`}>
           <Card.Title as='div'>
@@ -20,7 +19,6 @@ const Product = ({ product }) => {
             {product.rating} from {product.numReviews} reviews
           </div>
         </Card.Text>
-
         <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
     </Card>
