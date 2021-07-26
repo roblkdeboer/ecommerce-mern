d051254f7e94e0f4aeda16f2188f23ed5acce41c
Rob de Boer <roblkdeboer@gmail.com>
2021-07-26 17:44:52 +0800

    Add rating components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -10,7 +10,8 @@ const HomeScreen = () => {
       <Row>
         {/* For each product, what do I want to show */}
         {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
+          // Need to add an unique key to filter through (_id)
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             {/* Pass in each product from the map to render each of the products */}
             <Product product={product} />
           </Col>
