dbcb2d4d386d64058eb30adcdf8d2cd5b4e06c4f
roblkdeboer <roblkdeboer@gmail.com>
2021-07-27 23:43:17 +0800

    Connect MongoDB to server

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+// Use async function to handle a returned promise
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    // Exit with failure
+    process.exit(1);
+  }
+};
+
+export default connectDB;
