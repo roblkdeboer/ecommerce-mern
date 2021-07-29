3a538f98680e8e252dfd6603d9cedf3d469f409c
roblkdeboer <roblkdeboer@gmail.com>
2021-07-29 13:19:40 +0800

    Add error handler for wrong products in API

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -28,7 +28,9 @@ router.get(
     if (product) {
       res.json(product);
     } else {
-      res.status(404).json({ message: 'Product not found' });
+      // Error throws when a formated objectID is not valid
+      res.status(404);
+      throw new Error('Product not found');
     }
   })
 );
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -17,10 +18,17 @@ app.get('/', (req, res) => {
 // For anything directed to products, send it to the product router
 app.use('/api/products', productRoutes);
 
+// If someone tries to access an endpoint that is not defined
+app.use(notFound);
+// Writing custom middleware to intercept and generate error responses
+app.use(errorHandler);
+
 // If port isn't found in .env file, use 5000
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
