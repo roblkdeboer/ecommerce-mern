c1df6ddc0c0bacf88d34f14d74974f65c7f16318
Rob de Boer <roblkdeboer@gmail.com>
2021-10-13 23:40:08 +0800

    Create add to cart functionality

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -17,7 +17,7 @@ import { listProductDetails } from '../actions/productActions';
 
 // Match comes from Router
 const ProductScreen = ({ history, match }) => {
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
   const dispatch = useDispatch();
   // Takes in state and what part of the state we want to select
   const productDetails = useSelector((state) => state.productDetails);
