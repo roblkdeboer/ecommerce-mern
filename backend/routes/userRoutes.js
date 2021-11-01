67be90416f76255bd053bd1f08667ccf81e6d5e0
Rob de Boer <roblkdeboer@gmail.com>
2021-11-02 00:11:31 +0800

    Adding authentication middleware to protect the profile route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const router = express.Router();
 import { authUser, getUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 // Post request to login and call authUser
 router.post('/login', authUser);
 
-router.route('/profile').get(getUserProfile);
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
