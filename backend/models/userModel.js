49334c4441f63b91c941319f1b0df14513590a91
Rob de Boer <roblkdeboer@gmail.com>
2021-11-02 00:36:42 +0800

    User registration and password encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -32,6 +32,16 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// Middleware to happen before save which will salt and hash the password if the password hasn't been modified
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,18 @@
 import express from 'express';
 const router = express.Router();
-import { authUser, getUserProfile } from '../controllers/userController.js';
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 // Post request to login and call authUser
 router.post('/login', authUser);
+// Post request to create a new user
+router.route('/').post(registerUser);
 
+// Post request to return a logged in user
 router.route('/profile').get(protect, getUserProfile);
 
 export default router;
