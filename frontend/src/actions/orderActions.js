71c907bd4ef42517a62fb56d06981ce84a6b3b95
Rob de Boer <roblkdeboer@gmail.com>
2021-12-19 16:53:23 +0800

    Create order pay reducer and action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -5,6 +5,9 @@ import {
   ORDER_DETAILS_FAIL,
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
+  ORDER_PAY_FAIL,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
 } from '../constants/orderConstants';
 import axios from 'axios';
 
@@ -85,3 +88,47 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
     });
   }
 };
+
+// use getState to get userinfo to get the token to authorise request
+export const payOrder =
+  (orderId, paymentResult) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: ORDER_PAY_REQUEST,
+      });
+
+      // Get userInfo in userLogin state
+      const {
+        userLogin: { userInfo },
+      } = getState();
+
+      // Send the token for authorization
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+
+      // Data to be updated with is the specific order object
+      const { data } = await axios.put(
+        `/api/orders/${orderId}/pay`,
+        paymentResult,
+        config
+      );
+
+      dispatch({
+        type: ORDER_PAY_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: ORDER_PAY_FAIL,
+        //   Return error response and if there's a specific error message, return it, if not, return the error message
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -16,6 +16,7 @@ import {
 import {
   orderCreateReducer,
   orderDetailsReducer,
+  orderPayReducer,
 } from './reducers/orderReducers';
 
 const reducer = combineReducers({
@@ -28,6 +29,7 @@ const reducer = combineReducers({
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
 });
 
 // If found in local storage, parse the JSON, if not, return an empty array
