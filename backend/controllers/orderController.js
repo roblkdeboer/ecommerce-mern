abf9be5a0b82e776b5ad755f589ac02404828ef3
Rob de Boer <roblkdeboer@gmail.com>
2021-12-19 16:43:16 +0800

    Create update order to paid endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -58,4 +58,31 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById };
+// @desc Update order to paid
+// @route PUT /api/orders/:id/pay
+// @access Private
+// Use async function as mongoose returns a promise
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  // Attach the name and email for the order from the user table
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,6 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { addOrderItems, getOrderById } from '../controllers/orderController.js';
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 // Post request to create a new user
@@ -8,5 +12,8 @@ router.route('/').post(protect, addOrderItems);
 
 // Get request to get an order by id
 router.route('/:id').get(protect, getOrderById);
+
+// Update order to paid
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 export default router;
