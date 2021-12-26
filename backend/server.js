1fe27f2b05974a6d092b55ebd26b264bd3ef1a2d
Rob de Boer <roblkdeboer@gmail.com>
2021-12-26 21:49:47 +0800

    Add morgan for HTTP logging in dev mode

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import express from 'express';
+import morgan from 'morgan';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
@@ -17,6 +18,10 @@ const app = express();
 
 // Allow us to accept JSON data in the request body
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.get('/', (req, res) => {
   res.send('API is running');
