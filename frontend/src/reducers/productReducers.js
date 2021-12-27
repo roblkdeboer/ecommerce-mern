6ee5a40c971b7ea7cab5be0a334290341793dc07
Rob de Boer <roblkdeboer@gmail.com>
2021-12-27 16:51:30 +0800

    Create top products action

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -20,6 +20,9 @@ import {
   PRODUCT_CREATE_REVIEW_SUCCESS,
   PRODUCT_CREATE_REVIEW_FAIL,
   PRODUCT_CREATE_REVIEW_RESET,
+  PRODUCT_TOP_FAIL,
+  PRODUCT_TOP_REQUEST,
+  PRODUCT_TOP_SUCCESS,
 } from '../constants/productConstants';
 
 // Reducers take in an initial state and an action
@@ -126,6 +129,22 @@ export const productReviewCreateReducer = (state = {}, action) => {
       return { loading: false, error: action.payload };
     case PRODUCT_CREATE_REVIEW_RESET:
       return { product: {} };
+    default:
+      return state;
+  }
+};
+
+// Reducers take in an initial state and an action
+// Handle certain cases for the product object  (request product, return product, can't retrieve product)
+export const productTopRatedReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    case PRODUCT_TOP_REQUEST:
+      // Spread the state to use whatever is in the current state
+      return { loading: true, products: [] };
+    case PRODUCT_TOP_SUCCESS:
+      return { loading: false, products: action.payload };
+    case PRODUCT_TOP_FAIL:
+      return { loading: false, error: action.payload };
     default:
       return state;
   }
