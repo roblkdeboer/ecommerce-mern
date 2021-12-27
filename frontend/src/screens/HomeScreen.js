6b6b84a58f24ac173f09f19915cfa7a363e1a899
Rob de Boer <roblkdeboer@gmail.com>
2021-12-27 16:31:23 +0800

    Add page numbers for product list

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,23 +3,25 @@ import { useDispatch, useSelector } from 'react-redux';
 import Product from '../components/Product';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
+import Paginate from '../components/Paginate';
 import { Row, Col } from 'react-bootstrap';
 import { listProducts } from '../actions/productActions';
 
 const HomeScreen = ({ match }) => {
   const keyword = match.params.keyword;
+  const pageNumber = match.params.pageNumber || 1;
 
   const dispatch = useDispatch();
   // Takes in state and what part of the state we want to select
   const productList = useSelector((state) => state.productList);
-  const { loading, error, products } = productList;
+  const { loading, error, products, pages, page } = productList;
 
   //   Run when the component loads
   useEffect(() => {
     // Dispatch the listProducts action
-    dispatch(listProducts(keyword));
+    dispatch(listProducts(keyword, pageNumber));
     // Pass in an array of dependencies to trigger useEffect when it changes
-  }, [dispatch, keyword]);
+  }, [dispatch, keyword, pageNumber]);
 
   return (
     <>
@@ -30,16 +32,23 @@ const HomeScreen = ({ match }) => {
       ) : error ? (
         <Message variant="danger">{error}</Message>
       ) : (
-        <Row>
-          {/* For each product, what do I want to show */}
-          {products.map((product) => (
-            // Need to add an unique key to filter through (_id)
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              {/* Pass in each product from the map to render each of the products */}
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
+        <>
+          <Row>
+            {/* For each product, what do I want to show */}
+            {products.map((product) => (
+              // Need to add an unique key to filter through (_id)
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                {/* Pass in each product from the map to render each of the products */}
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ''}
+          />
+        </>
       )}
     </>
   );
